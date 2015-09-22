84ea40295b0d373ef6b9f4bb847f42eb8c1dfabc
Ram Sastry <csastry12@yahoo.com>
2015-09-21 17:01:13 -0700

    Orphan pages
    TODO filled in

--- MP2/OrphanPages.java
+++ MP2/OrphanPages.java
@@ -1,4 +1,5 @@
 import org.apache.hadoop.conf.Configuration;
+
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -11,6 +12,9 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+
+import TitleCount.TitleCountMap;
+import TitleCount.TitleCountReduce;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -25,21 +29,62 @@ public class OrphanPages extends Configured implements Tool {
 // <<< Don't Change
 
     @Override
-    public int run(String[] args) throws Exception {
+    public int run(String[] args) throws Exception 
+    {
         //TODO
+    	
+    	Job job = Job.getInstance(this.getConf(), "Orphan Pages");
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(LinkCountMap.class);
+        job.setReducerClass(OrphanPageReduce.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setJarByClass(OrphanPages.class);
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 
     public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
         @Override
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
+        {
             //TODO
+        	
+        	String line = value.toString();
+    //    	StringTokenizer tokenizer = new StringTokenizer(line, delimiters);
+        	String[] tokens = line.split(": | ");
+        	
+        	context.write(new Text(tokens[0]), new IntWritable(0));
+        	
+        	for (int i = 1; i < tokens.length; i++)
+        	{
+        		context.write(new Text(tokens[i]), new IntWritable(1));
+        	} 	
         }
     }
 
     public static class OrphanPageReduce extends Reducer<IntWritable, IntWritable, IntWritable, NullWritable> {
         @Override
-        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
+        {
             //TODO
+        	
+        	int sum = 0;
+            for (IntWritable val : values) 
+            {
+                sum += val.get();
+            }
+            
+            if(sum == 0)
+            {
+            	context.write(key, NullWritable.get());
+            }
         }
     }
 }
